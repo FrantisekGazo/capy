d2a1877a114a6b90c6763755db28528abf339e89
František Gažo <frantisek.gazo.313@gmail.com>
2017-10-20 14:18:10 +0200

    Fix calabash check

--- capy/capy.py
+++ capy/capy.py
@@ -47,7 +47,7 @@ def check_package(name, current_version):
 
 
 def check_calabash():
-    cmds = ['bundle', 'exec', 'calabash-android', 'cucumber']
+    cmds = ['calabash-android', 'cucumber']
     for cmd in cmds:
         if not check_cmd(cmd):
             exit_error('Command %s was NOT found. Please make sure calabash is installed.' % cmd)
